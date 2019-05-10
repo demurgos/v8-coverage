4a484f36507ce85a56d8ee3312a02d064ee8bc31
Charles Samborski <demurgos@demurgos.net>
2019-05-11 00:34:22 +0200

    chore: Update build tools

--- ts/gulpfile.ts
+++ ts/gulpfile.ts
@@ -1,30 +1,30 @@
-import * as buildTools from "turbo-gulp";
-import { LibTarget, registerLibTasks } from "turbo-gulp/targets/lib";
-import { MochaTarget, registerMochaTasks } from "turbo-gulp/targets/mocha";
-
 import gulp from "gulp";
 import minimist from "minimist";
+import * as buildTools from "turbo-gulp";
+import { Project } from "turbo-gulp/project";
+import { LibTarget, registerLibTasks } from "turbo-gulp/targets/lib";
+import { MochaTarget, registerMochaTasks } from "turbo-gulp/targets/mocha";
 
 interface Options {
   devDist?: string;
 }
 
 const options: Options & minimist.ParsedArgs = minimist(process.argv.slice(2), {
-  string: ["devDist"],
-  default: {devDist: undefined},
-  alias: {devDist: "dev-dist"},
+  string: ["next"],
+  default: {next: undefined},
 });
 
-const project: buildTools.Project = {
+const project: Project = {
   root: __dirname,
   packageJson: "package.json",
   buildDir: "build",
   distDir: "dist",
   srcDir: "src",
-  typescript: {
-    compilerOptions: {
-      allowSyntheticDefaultImports: true,
-      esModuleInterop: true,
+  tslint: {
+    configuration: {
+      rules: {
+        whitespace: false,
+      },
     },
   },
 };
@@ -37,7 +37,7 @@ const lib: LibTarget = {
   mainModule: "index",
   dist: {
     packageJsonMap: (old: buildTools.PackageJson): buildTools.PackageJson => {
-      const version: string = options.devDist !== undefined ? `${old.version}-build.${options.devDist}` : old.version;
+      const version: string = options.next !== undefined ? `${old.version}-build.${options.next}` : old.version;
       return <any> {...old, version, scripts: undefined, private: false};
     },
     npmPublish: {
@@ -56,11 +56,6 @@ const lib: LibTarget = {
       branch: "gh-pages",
     },
   },
-  copy: [
-    {
-      files: ["**/*.json"],
-    },
-  ],
   clean: {
     dirs: ["build/lib", "dist/lib"],
   },
@@ -70,22 +65,11 @@ const test: MochaTarget = {
   project,
   name: "test",
   srcDir: "src",
-  scripts: ["test/**/*.ts", "lib/**/*.ts", "e2e/*/*.ts"],
+  scripts: ["test/**/*.ts", "lib/**/*.ts"],
   customTypingsDir: "src/custom-typings",
   tscOptions: {
-    allowSyntheticDefaultImports: true,
-    esModuleInterop: true,
     skipLibCheck: true,
   },
-  // generateTestMain: true,
-  copy: [
-    {
-      src: "e2e",
-      // <project-name>/(project|test-resources)/<any>
-      files: ["*/project/**/*", "*/test-resources/**/*"],
-      dest: "e2e",
-    },
-  ],
   clean: {
     dirs: ["build/test"],
   },
