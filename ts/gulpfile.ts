a6243a02d9ee967d59fdfb3f77eeafad6ac9542d
Charles Samborski <demurgos@demurgos.net>
2021-09-25 13:59:26 +0200

    [all] Fix Github Actions workflows
    - Fix branch filter
    - Fix self-link to the `@c88/v8-coverage` package
    - Fix ts tests hanging waiting for the inspector

--- ts/gulpfile.ts
+++ ts/gulpfile.ts
@@ -3,7 +3,7 @@ import minimist from "minimist";
 import * as buildTools from "turbo-gulp";
 import { Project } from "turbo-gulp/project";
 import { LibTarget, registerLibTasks } from "turbo-gulp/targets/lib";
-import { MochaTarget, registerMochaTasks } from "turbo-gulp/targets/mocha";
+import { MochaTarget, MochaTasks, registerMochaTasks } from "turbo-gulp/targets/mocha";
 
 interface Options {
   next?: string;
@@ -76,6 +76,10 @@ const test: MochaTarget = {
 };
 
 const libTasks: any = registerLibTasks(gulp, lib);
+const testTasks: MochaTasks = registerMochaTasks(gulp, test);
+
+gulp.task("test-no-coverage", gulp.series(testTasks.clean!, testTasks.build, testTasks.runCjs!));
+
 registerMochaTasks(gulp, test);
 buildTools.projectTasks.registerAll(gulp, project);
 
